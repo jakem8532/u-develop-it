b390dace114b214ceeb9bae75cda41150ca67970
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-07-23 14:23:37 -0400

    connected mysql to database

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express')
+const mysql = require('mysql2')
 
 const PORT = process.env.PORT || 3001
 const app = express()
 
+
+// Express Middleware
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MYSQL password
+        password: 'charlotte',
+        database: 'election'
+    },
+
+    console.log('Connected to the election database')
+)
+
+// default response for other requests (Not Found)
 app.use((req, res) => {
     res.status(404).end()
 })
